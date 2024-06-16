e19efb7e0e08e1dc0ff72e2ff28aaa3fdab98a91
Damir <damir.zh4@gmail.com>
2024-06-16 23:41:46 +0500

    added router, header, pages and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <a href="http://calculator">calculator</a>
+        </div>
+		)
+}
+
+export default Header
---
+++ src/pages/ConverterCalculator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ConverterCalculator() {
+    return (
+        <div>
+            <h1>calculator</h1>
+        </div>
+    )
+}
+
+export default ConverterCalculator

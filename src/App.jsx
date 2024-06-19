7108ca1992f5d0ca6c192747be4b371dc6834e70
Damir <damir.zh4@gmail.com>
2024-06-20 01:49:08 +0500

    added start script, fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from 'antd'
-import { Content, Header, Footer } from 'antd/es/layout/layout'
+import { Content, Header } from 'antd/es/layout/layout'
 import Home from './components/pages/Home/Home'
 import Converter from './components/pages/Converter/Converter'
 import MyHeader from './components/Header/Header'
